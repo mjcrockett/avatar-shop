8db7fff1fda318106883cd5ef38bc1a305df83a9
Michael Crockett <mcrockett@cmgfi.com>
2025-09-30 17:11:38 -0700

    Started banner

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CategoryComponent } from './category/category.component';
 import { Categories } from './models';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [CategoryComponent, CommonModule],
+  imports: [CategoryComponent, CommonModule, BannerComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
